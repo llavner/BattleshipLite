0174ded3269b445ba056ed4035b900ca00553fd5
llavner <marcus.renvall@gmail.com>
2025-07-30 07:57:13 +0200

    added game loop and creation of the player

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,3 +1,38 @@
 ﻿
+using BattleshipLite;
+using BattleshipLiteLibrary;
+using BattleshipLiteLibrary.Models;
 
+ConsoleMessages.WelcomeMessage();
+
+PlayerModel activePlayer = ConsoleLogic.CreatePlayer("Player 1");
+
+PlayerModel opponent = ConsoleLogic.CreatePlayer("Player 2");
+
+PlayerModel winner = null;
+
+do
+{
+    ConsoleLogic.DisplayShotGrid(activePlayer);
+
+    ConsoleLogic.RecordPlayerShot(activePlayer, opponent);
+
+    bool doesGameContinue = GameLogic.PlayerStillActive(opponent);
+
+    if (doesGameContinue == true)
+    {
+        // Swap positions, using tuple .Net 7+
+        (activePlayer, opponent) = (opponent, activePlayer);
+    }
+    else
+    {
+        winner = activePlayer;
+    }
+
+    ConsoleLogic.IdentifyWinner(winner);
+
+
+} while (winner == null);
+
+Console.ReadLine();
 
