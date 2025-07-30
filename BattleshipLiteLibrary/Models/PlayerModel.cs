5ce9700c2d34b1583142422604a4d4081ba1c732
llavner <marcus.renvall@gmail.com>
2025-07-30 07:55:42 +0200

    Created the list when PlayerModel class is instansiated

--- BattleshipLiteLibrary/Models/PlayerModel.cs
+++ BattleshipLiteLibrary/Models/PlayerModel.cs
@@ -3,7 +3,7 @@
 public class PlayerModel
 {
     public string PlayerName { get; set; }
-    public List<GridSpotModel> ShipLocations { get; set; }
-    public List<GridSpotModel> ShotGrid { get; set; }
+    public List<GridSpotModel> ShipLocations { get; set; } = new();
+    public List<GridSpotModel> ShotGrid { get; set; } = new();
 
 }
